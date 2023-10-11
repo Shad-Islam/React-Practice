7004eab8e1a508a1ae8e4d22c3933783c2443acb
Shad-islam <shadislam48@gmail.com>
2023-10-11 21:39:37 +0600

    Add counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Counter from "./components/Counter";
 import Test from "./components/TestComponent";
 
 function App() {
   return (
     <>
       <Test />
+      <Counter />
     </>
   );
 }
