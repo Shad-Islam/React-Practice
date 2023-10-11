b682be461ec99ab06d74725ee10a1a2ed0a66ae7
Shad-islam <shadislam48@gmail.com>
2023-10-11 22:49:02 +0600

    Set line and button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Counter() {
-  return <div>Counter</div>;
+  let counter = 0;
+  return (
+    <div>
+      <h1>The current value of counter is {counter}</h1>
+      <button>Increase by 1</button>
+      <button>decrease by 1</button>
+    </div>
+  );
 }
 
 export default Counter;
