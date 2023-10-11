cda217092c02ccef66afffeeded7f58f187aee5a
Shad-islam <shadislam48@gmail.com>
2023-10-11 21:41:39 +0600

    Remove test component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import Counter from "./components/Counter";
-import Test from "./components/TestComponent";
 
 function App() {
   return (
     <>
-      <Test />
       <Counter />
     </>
   );
