3cccf2d18de28f4c8f9f71b4919acd9c0322da45
Shad-islam <shadislam48@gmail.com>
2023-10-12 21:41:57 +0600

    Add another 2 button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,23 +3,24 @@ import React, { useState } from "react";
 function Counter() {
   const [dynamicCounter, setdynamicCounter] = useState(0);
 
-  const increaseHandler = () => {
-    setdynamicCounter(dynamicCounter + 1);
+  const increaseHandler = (props) => {
+    setdynamicCounter(dynamicCounter + props);
     console.log(dynamicCounter);
   };
 
-  const decreaseHandler = () => {
-    setdynamicCounter(dynamicCounter - 1);
+  const decreaseHandler = (props) => {
+    setdynamicCounter(dynamicCounter - props);
     console.log(dynamicCounter);
   };
 
   return (
     <div className="counter">
       <h1>The current value of counter is {dynamicCounter}</h1>
-      <button onClick={increaseHandler} className="counter__btn">
-        Increase by 1
-      </button>
-      <button onClick={decreaseHandler}>decrease by 1</button>
+      <button onClick={() => increaseHandler(1)}>Increase by 1</button>
+      <button onClick={() => decreaseHandler(1)}>decrease by 1</button>
+
+      <button onClick={() => increaseHandler(5)}>Increase by 5</button>
+      <button onClick={() => decreaseHandler(5)}>decrease by 5</button>
     </div>
   );
 }
