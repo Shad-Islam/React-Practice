790ab37853725bdf734320307be73681e5fdc014
Shad-islam <shadislam48@gmail.com>
2023-10-13 02:26:29 +0600

    Add view part

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [notes, setNotes] = useState([
+    { id: 1, title: "My first note" },
+    { id: 2, title: "My second note" },
+  ]);
+  return (
+    <>
+      <form>
+        <input type="text" />
+        <button type="submit">Add Note</button>
+      </form>
+      <ul className="note-list">
+        {notes.map((note) => (
+          <li>
+            <span>{note.title}</span>
+            <button>Edit</button>
+            <button>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
