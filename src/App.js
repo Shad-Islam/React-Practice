362727bde7957a81df67b8bf9ad3bd1a2fe29243
Shad-islam <shadislam48@gmail.com>
2023-10-13 06:15:44 +0600

    Add create handler

--- src/App.js
+++ src/App.js
@@ -2,19 +2,40 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [noteTitle, setNotetitle] = useState("");
   const [notes, setNotes] = useState([
     { id: 1, title: "My first note" },
     { id: 2, title: "My second note" },
   ]);
+
+  const createHandler = (event) => {
+    event.preventDefault();
+    const newNote = {
+      id: Date.now + "",
+      title: noteTitle,
+    };
+    setNotes([...notes, newNote]);
+  };
+  
+  const removeHandler = () => {};
   return (
     <>
-      <form>
-        <input type="text" />
+      <form onSubmit={createHandler}>
+        <input
+          type="text"
+          value={noteTitle}
+          onChange={(event) => {
+            setNotetitle(event.target.value);
+          }}
+        />
+        {/* {console.log({ noteTitle })}
+        {console.log({ notes })} */}
+
         <button type="submit">Add Note</button>
       </form>
       <ul className="note-list">
         {notes.map((note) => (
-          <li>
+          <li key={note.id}>
             <span>{note.title}</span>
             <button>Edit</button>
             <button>Remove</button>
